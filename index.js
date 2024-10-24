6c44c7dced268bad2c714a3e5a3b8ec8f94d64f9
rehanuddin1991 <rehan.ictd@gmail.com>
2024-10-24 20:45:12 +0600

    changed

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 const { MongoClient, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_SECRET}@cluster0.grsue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+//const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_SECRET}@cluster0.grsue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri=process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 async function run() {
   try {
