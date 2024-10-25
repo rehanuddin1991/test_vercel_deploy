4d161597ca60e850f35d2d0808a494115d64e2e0
rehanuddin1991 <rehan.ictd@gmail.com>
2024-10-25 18:52:37 +0600

    c

--- api/index.js
+++ api/index.js
@@ -3,9 +3,10 @@ const cors = require("cors");
 require('dotenv').config()
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+app.use('/route');
 const { MongoClient, ObjectId } = require('mongodb');
 //const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_SECRET}@cluster0.grsue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const uri=process.env.MONGODB_URI;
